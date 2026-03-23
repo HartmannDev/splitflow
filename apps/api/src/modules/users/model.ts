c2dd631ca4d89fb5d7eaadea676adb8db655b5b3
HartmannDev <dev.mghartmann@gmail.com>
2026-03-23 20:06:32 +1100

    Implement users endpoints

--- apps/api/src/modules/users/model.ts
+++ apps/api/src/modules/users/model.ts
@@ -1,5 +1,13 @@
 import z from 'zod'
 
+export const PasswordSchema = z
+	.string()
+	.min(8)
+	.regex(/[A-Z]/, { message: 'Must contain at least one uppercase letter' })
+	.regex(/[a-z]/, { message: 'Must contain at least one lowercase letter' })
+	.regex(/[0-9]/, { message: 'Must contain at least one number' })
+	.regex(/[^a-zA-Z0-9]/, { message: 'Must contain at least one special character' })
+
 export const UserSchema = z.object({
 	id: z.uuid(),
 	role: z.enum(['user', 'admin']),
@@ -28,13 +36,7 @@ export const CreateUserSchema = PublicUserSchema.omit({
 	updatedAt: true,
 	deletedAt: true,
 }).extend({
-	password: z
-		.string()
-		.min(8)
-		.regex(/[A-Z]/, { message: 'Must contain at least one uppercase letter' })
-		.regex(/[a-z]/, { message: 'Must contain at least one lowercase letter' })
-		.regex(/[0-9]/, { message: 'Must contain at least one number' })
-		.regex(/[^a-zA-Z0-9]/, { message: 'Must contain at least one special character' }),
+	password: PasswordSchema,
 })
 
 export const DeleteUserResponseSchema = z.object({
@@ -44,6 +46,7 @@ export const DeleteUserResponseSchema = z.object({
 export const UserListSchema = z.array(PublicUserSchema)
 export const UserIDSchema = UserSchema.pick({ id: true })
 
+export type Password = z.infer<typeof PasswordSchema>
 export type User = z.infer<typeof UserSchema>
 export type PublicUser = z.infer<typeof PublicUserSchema>
 export type CreateUserInput = z.infer<typeof CreateUserSchema>
