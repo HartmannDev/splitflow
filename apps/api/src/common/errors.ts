8e2df1145db0c0b01083bb3f84528a079f79681e
HartmannDev <dev.mghartmann@gmail.com>
2026-03-26 19:04:15 +1100

    Refactor error structure

--- apps/api/src/common/errors.ts
+++ apps/api/src/common/errors.ts
@@ -1,28 +1,45 @@
 import type { DatabaseError } from 'pg'
 
-export const isDatabaseError = (error: unknown): error is DatabaseError =>
-	typeof error === 'object' && error !== null && 'code' in error
+export const AppError = () => {
+	const isDatabaseError = (error: unknown): error is DatabaseError =>
+		typeof error === 'object' && error !== null && 'code' in error
 
-export const conflictError = (message: string) =>
-	Object.assign(new Error(message), {
-		name: 'ConflictError',
-		statusCode: 409,
-	})
+	const conflictError = (message: string) =>
+		Object.assign(new Error(message), {
+			name: 'ConflictError',
+			statusCode: 409,
+		})
 
-export const notFoundError = (message: string) =>
-	Object.assign(new Error(message), {
-		name: 'Not Found',
-		statusCode: 404,
-	})
+	const notFoundError = (message: string) =>
+		Object.assign(new Error(message), {
+			name: 'Not Found',
+			statusCode: 404,
+		})
 
-export const unauthorizedError = (message: string) =>
-	Object.assign(new Error(message), {
-		name: 'Unauthorized',
-		statusCode: 401,
-	})
+	const unauthorizedError = (message: string) =>
+		Object.assign(new Error(message), {
+			name: 'Unauthorized',
+			statusCode: 401,
+		})
 
-export const forbiddenError = (message: string) =>
-	Object.assign(new Error(message), {
-		name: 'Forbidden',
-		statusCode: 403,
-	})
+	const forbiddenError = (message: string) =>
+		Object.assign(new Error(message), {
+			name: 'Forbidden',
+			statusCode: 403,
+		})
+
+	const badRequestError = (message: string) =>
+		Object.assign(new Error(message), {
+			name: 'Bad Request',
+			statusCode: 400,
+		})
+
+	return {
+		isDatabaseError,
+		conflictError,
+		notFoundError,
+		unauthorizedError,
+		forbiddenError,
+		badRequestError,
+	}
+}
